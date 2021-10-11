7e9f47ae7b261eba2498c674e754ba2cc8da0305
tmb19188 <mark.mccafferty.2019@uni.strath.ac.uk>
2021-10-11 17:51:15 +0100

    Look, it's muh password

--- cjghffnycduqvnm/framing.php
+++ cjghffnycduqvnm/framing.php
@@ -7,5 +7,9 @@
 <body>
 <h1>Frame Price Estimator</h1>
 <p>Please enter your photo sizes to get a framing cost estimate</p>
+<?php
+echo "Quick test"
+?>
 </body>
 </html>
+
--- cjghffnycduqvnm/password.php
+++
@@ -1,9 +0,0 @@
-<?php
-Function get_password(){
-    if (strpos($_SERVER['PHP_SELF'], "~tmb19188/") === 0)
-    {
-        return "LolXDBro";
-    }else {
-        die("Access Denied Nerd!");
-    }
-}
